3e3e344d44088a38b3d6646fc2377d0896b45c95
neisser <neisser2909@gmail.com>
2025-06-02 10:11:47 -0400

    Implementado Rest API

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Query, Post, Get, Put, Delete, Body, Param } from '@nestjs/common';
+import { TaskService } from './task.service';
+import { TaskDTO } from './dto/task.dto';
 
 @Controller('api/v1/task')
-export class TaskController {}
+export class TaskController {
+    constructor(private readonly taskService: TaskService) {}
+
+    @Post()
+    create(@Body() taskDto: TaskDTO) {
+        return this.taskService.create(taskDto);
+    }
+
+    @Get()
+    findAll() {
+        return this.taskService.findAll(); 
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.taskService.findOne(id); 
+    }
+
+    @Put(':id')
+    update(@Param('id') id: string, @Body() taskDto: TaskDTO) {
+        return this.taskService.update(id, taskDto); 
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id: string) {
+        return this.taskService.delete(id); 
+    }
+}
